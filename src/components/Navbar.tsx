3953c7c18d721d0b268df1a888008b298d028e3a
Apoorva Bairi <moanony49@gmail.com>
2026-04-21 15:48:41 +0530

    Updated navbar title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 export default function Navbar() {
   return (
     <div className="flex justify-between p-4 bg-white dark:bg-gray-900 text-black dark:text-white">
-      <h1 className="font-bold">MovieApp</h1>
+      <h1 className="font-bold">Movie Review</h1>
 
       <div className="flex gap-4 items-center">
         <Link to="/">Home</Link>
